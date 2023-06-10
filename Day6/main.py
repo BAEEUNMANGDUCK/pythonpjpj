5a917a6eacb28824d79f65ecadee05a6b7a55709
BAEEUNMANGDUCK <122082279+BAEEUNMANGDUCK@users.noreply.github.com>
2023-06-10 14:35:26 +0900

    Day6 for and while loop
    Learning for loops and while loops using Reeborg's hurdle website

---
+++ Day6/main.py
@@ -0,0 +1,22 @@
+# function
+
+# Built-in function
+
+# print("Hello")
+# num_char = len("Hello")
+
+
+# 사용자 정의 함수(Defining Functions) => 들여 쓰기(Indentation)가 중요함
+def my_function():
+    print("Hello")
+    print("Bye")
+
+# 함수 호출(Calling Functions)
+my_function()
+
+
+# While 반복문 => 조건이 참일 동안 계속 반복
+# for문은 주로 반복가능한 객체를 이용할 때 유용함.
+# while문은 무한 루프에 빠지기 쉬움
+
+# for 문 while 문 예제는 Reeborg's Hurdle로 대체 
